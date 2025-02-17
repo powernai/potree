906986ffcfe6e5c4d97c474783df6a3998fe63c4
Nathan Turpin <nathan.turpin@powern.ai>
2025-02-17 10:12:29 -0500

    debugging

--- src/navigation/OrbitControls.js
+++ src/navigation/OrbitControls.js
@@ -145,6 +145,8 @@ export class OrbitControls extends EventDispatcher {
 
     this.touchMove = (e) => {
 	  if (!this.scissorZoneIdxs.includes(e.scissorZoneIdx)) {
+		console.debug(this.scissorZoneIdxs);
+		console.debug(e.scissorZoneIdx);
 		return;
 	  }
       if (e.touches.length === 2 && previousTouch.touches.length === 2) {
